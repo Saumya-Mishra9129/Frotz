Saumya-Mishra9129/Frotz

commit 480c2079cdc483c7ea815bd0fd9e7cd23f752e81

Fix Indentation

frotz.py
@@ -139,15 +139,17 @@ class FrotzActivity(activity.Activity):
                 logging.debug('Using frotz installed in the system')
                 self._vte.feed_child(
                     (
-                            "cd '%s'; "
-                            "clear; "
-                            "frotz|head -3 ; "
-                            "echo '\nLoading %s...'; "
-                            "sleep 2; frotz '%s'; "
-                            "exit\n" % (
-                                save_dir,
-                                os.path.basename(game_file),
-                                game_file)).encode('utf-8')
+                        "cd '%s'; "
+                        "clear; "
+                        "frotz|head -3 ; "
+                        "echo '\nLoading %s...'; "
+                        "sleep 2; frotz '%s'; "
+                        "exit\n" % (
+                            save_dir,
+                            os.path.basename(game_file),
+                            game_file)
+
+                    ).encode('utf-8')
                 )
             else:
                 dialog = Gtk.MessageDialog(

commit c7558d0d386b00e7b5438504690fd153ae994e7f

Remove empty line caused in https://github.com/sugarlabs/Frotz/pull/7/commits/480c2079cdc483c7ea815bd0fd9e7cd23f752e81

frotz.py
@@ -154,7 +154,6 @@ class FrotzActivity(activity.Activity):
                             save_dir,
                             os.path.basename(game_file),
                             game_file)
-
                     ).encode('utf-8')
                 )
             else: