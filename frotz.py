2b4038e1b735559fc780b0d36cc0428e0261af1b
James Cameron <quozl@laptop.org>
2020-08-20 13:11:33 +1000

    Review - reapply license
    Reapply the license to the program, see "How to Apply These Terms to
    Your New Programs" in COPYING.

--- frotz.py
+++ frotz.py
@@ -1,26 +1,23 @@
-# Frotz.activity
-# A simple front end to the classic interactive fiction interpreter frotz on the XO laptop
-# http://wiki.laptop.org/go/Frotz
+# Frotz activity, wrapper for the frotz interactive fiction interpreter
 #
 # Copyright (C) 2008  Joshua Minor
-# This file is part of Frotz.activity
 #
-# Parts of Frotz.activity are based on code from Terminal.activity
-# Copyright (C) 2007, Eduardo Silva <edsiper@gmail.com>.
-# Copyright (C) 2008, One Laptop Per Child
-# 
-#     Frotz.activity is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
+# Parts of Frotz activity are based on code from Terminal activity
+# Copyright (C) 2007  Eduardo Silva <edsiper@gmail.com>.
+# Copyright (C) 2008  One Laptop per Child
 #
-#     Frotz.activity is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#     You should have received a copy of the GNU General Public License
-#     along with Frotz.activity.  If not, see <http://www.gnu.org/licenses/>.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import configparser
 import logging
